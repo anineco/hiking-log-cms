095a9cd317a5bbed5229d88dac47a997b5e09e02
Anineco Nyanta <anineco@nifty.com>
2024-12-14 05:27:46 +0900

    remove calculation of bbox

--- mkresource.pl
+++ mkresource.pl
@@ -1,62 +1,48 @@
 #!/usr/bin/env -S perl -CA
+# mkresource.pl - create resource JSON file
+
 use strict;
 use warnings;
 use utf8;
 use open qw(:utf8 :std);
 
 use Config::Tiny;
-use Data::Dumper;
-{
-  no warnings 'redefine';
-  *Data::Dumper::qquote = sub { return shift; };
-  $Data::Dumper::Useperl = 1;
-}
 use Encode;
 use File::Basename;
 use Image::ExifTool qw(ImageInfo);
 use JSON;
 use List::Util qw(min);
-use POSIX qw(round hypot DBL_MAX);
+use POSIX qw(round hypot Inf);
 use Time::Piece;
 use Time::Seconds;
 use XML::Simple qw(:strict);
 
-#
-# 設定ファイルを読み込む
-#
+use constant ICON_SUMMIT => 952015; # kashmir3d:icon
+
+# load configuration file
 my $cf = Config::Tiny->read('config.ini');
 my $content = $cf->{_}->{content};
 my $material_gpx = $cf->{material}->{gpx};
 my $material_img = $cf->{material}->{img};
 
-#
-# コマンド行オプション
-#
-if ($#ARGV < 0) {
+# parse command line argument
+if ($#ARGV != 1) {
   my $script = basename($0);
-  print STDERR "Usage: $script <CID> [<title>]\n";
-  exit 1;
+  die "Usage: $script <CID> <title>";
 }
-my $cid = $ARGV[0]; # コンテントID
-my $title;
-if ($#ARGV > 0) {
-  $title = $ARGV[1];
-}
+my $cid = $ARGV[0]; # content ID
+my $title = $ARGV[1];
 
-#
-# 山行記録
-#
-my $resource = { cid => $cid };
+my $resource = { cid => $cid, title => $title };
 
-#
-# GPXファイルを読み込む
-#
+# load GPX files
 my $xs = XML::Simple->new(
   ForceArray => 1,
   KeepRoot => 1,
   KeyAttr => [],
   XMLDecl => '<?xml version="1.0" encoding="UTF-8"?>'
 );
+
 
 sub read_gpx {
   my ($file, $trkpts, $wpts) = @_;
@@ -69,10 +55,10 @@ sub read_gpx {
       icon => 0 + $wpt->{extensions}[0]->{'kashmir3d:icon'}[0],
       name => $wpt->{name}[0]
     };
-    if ($item->{icon} == 952015) { # 山頂
+    if ($item->{icon} == ICON_SUMMIT) {
       foreach my $cmt (split /,/, $wpt->{cmt}[0]) {
         my ($key, $value) = split /=/, $cmt;
-        if ($key eq '標高') {
+        if ($key eq '標高') { # 'elevation'
           $item->{ele} = $value;
           last;
         }
@@ -84,7 +70,7 @@ sub read_gpx {
     foreach my $trkseg (@{$trk->{trkseg}}) {
       foreach my $trkpt (@{$trkseg->{trkpt}}) {
         my $t = Time::Piece->strptime($trkpt->{time}[0], '%FT%TZ');
-        $t += 9 * ONE_HOUR; # JST に変更
+        $t += 9 * ONE_HOUR; # transfer UTC to JST
         push @{$trkpts}, {
           lon => 0 + $trkpt->{lon},
           lat => 0 + $trkpt->{lat},
@@ -95,45 +81,25 @@ sub read_gpx {
   }
 }
 
+# extract session info. from GPS data
 sub read_section {
   my @files = @_; # GPX files
-  my ($max_lon, $min_lon, $max_lat, $min_lat) = (-180, 180, -90, 90);
-  my sub maxmin {
-    my ($lon, $lat) = @_;
-    $max_lon = $lon if ($max_lon < $lon);
-    $min_lon = $lon if ($min_lon > $lon);
-    $max_lat = $lat if ($max_lat < $lat);
-    $min_lat = $lat if ($min_lat > $lat);
-  }
-
   my @wpts = ();
   my @trkpts_unsorted = ();
 
+  # read track points and waypoints
   foreach my $file (@files) {
     read_gpx($file, \@trkpts_unsorted, \@wpts);
   }
-
   my @trkpts = sort { $a->{time} cmp $b->{time} } @trkpts_unsorted;
 
-  #
-  # 緯度・経度の最大・最小を求める
-  #
+  # for each track point, find the nearest waypoint
   foreach my $p (@trkpts) {
-    maxmin($p->{lon}, $p->{lat});
-  }
-  foreach my $p (@wpts) {
-    maxmin($p->{lon}, $p->{lat});
-  }
-  my $bound = [ $max_lon, $min_lon, $max_lat, $min_lat ];
-
-  #
-  # 最寄のウェイポイントを求める
-  #
-  foreach my $p (@trkpts) {
-    my $dmin = DBL_MAX;
+    my $dmin = Inf;
     my $n = -1;
     for (my $i = 0; $i <= $#wpts; $i++) {
       my $q = $wpts[$i];
+      # HACK: compute rough estimate of distance in degree
       my $d = hypot($q->{lon} - $p->{lon}, $q->{lat} - $p->{lat});
       if ($dmin > $d) {
         $dmin = $d;
@@ -144,25 +110,20 @@ sub read_section {
     $p->{n} = $n;
   }
 
-  #
-  # 平滑化
-  #
   for (my $i = 0; $i <= $#trkpts; $i++) {
     my $p0 = $trkpts[$i - ($i > 0)];
     my $p1 = $trkpts[$i];
     my $p2 = $trkpts[$i + ($i < $#trkpts)];
     my $nearest = -1;
     if ($p0->{n} == $p1->{n} && $p2->{n} == $p1->{n}) {
-      if (min($p0->{d}, $p1->{d}, $p2->{d}) < 0.0004) { # 約40m
+      if (min($p0->{d}, $p1->{d}, $p2->{d}) < 0.0004) { # NOTE: equivalent to 40m
         $nearest = $p1->{n};
       }
     }
     $p1->{nearest} = $nearest;
   }
 
-  #
-  # 行程を作成
-  #
+  #  create timeline
   my @timeline = ();
   my @summits = ();
 
@@ -185,7 +146,7 @@ sub read_section {
         name => $q->{name},
         timespan => [ $start, $end ]
       };
-      if ($q->{icon} == 952015) { # 山頂
+      if ($q->{icon} == ICON_SUMMIT) {
         if (exists $q->{ele}) {
           $item->{ele} = $q->{ele};
           push @summits, $q->{name};
@@ -195,59 +156,52 @@ sub read_section {
     }
   }
 
-  #
-  # セクションを作成
-  #
   my @t = split /T/, $timeline[0]->{timespan}[0];
   return {
     title => $title || join('〜', @summits),
     date => $t[0],
     timespan => [ $timeline[0]->{timespan}[0], $timeline[-1]->{timespan}[1] ],
-    timeline => \@timeline,
-    bound => $bound
+    timeline => \@timeline
   };
 }
 
-foreach my $map (glob "$material_gpx/$cid/routemap*.geojson") {
-  my $base = basename($map);
-  my $c = $base =~ s/^routemap(.*)\.geojson$/$1/r;
-  my @files = glob "$material_gpx/$cid/???$c.gpx";
+# set source GPX files and file name of routemap
+foreach my $track (glob "$material_gpx/$cid/trk*.gpx") {
+  next unless ($track =~ m|/trk([0-9]?)\.gpx$|);
+  my $c = $1;
+  my @files = glob "$material_gpx/$cid/???$c.gpx"; # rte, trk, wpt
   die 'no GPX files' if ($#files < 0);
   my $section = read_section(@files);
   $section->{gpx} = \@files;
   $section->{routemap} = "routemap$c.geojson";
   push @{$resource->{section}}, $section;
 }
+
+# set start and end date to resource
 if (exists $resource->{section}) {
-  $resource->{title} = join('・', map { $_->{title} } @{$resource->{section}});
-  $resource->{date} = { start => $resource->{section}[0]->{date}, end => $resource->{section}[-1]->{date} };
+  $resource->{date} = {
+    start => $resource->{section}[0]->{date},
+    end => $resource->{section}[-1]->{date}
+  };
 } else {
   my $ymd = $cid =~ s/(..)(..)(..)/20$1-$2-$3/r;
-  $resource->{title} = 'タイトル';
   $resource->{date} = { start => $ymd, end => $ymd };
   $resource->{section} = [ { timespan => [ $ymd . 'T00:00:00', $ymd . 'T23:59:59' ] } ];
 }
 
-#
-# 表紙画像
-#
+# set cover image to resource
 my %hash = ();
-my @covers = glob "$material_img/$cid/S[0-9][0-9][0-9][0-9].*";
-if ($#covers < 0) {
-  @covers = glob "$material_img/$cid/????[0-9][0-9][0-9][0-9](1).*";
-  die "no cover image" if ($#covers < 0);
-}
+my @covers = glob "$material_img/$cid/cover/*";
+die "no cover image" if ($#covers < 0);
 my $file = $covers[0];
 my $base = basename($file);
-my $key = $base =~ s/^.*([0-9]{4})(\(1\))?\..*$/$1/r;
+my $key = $base =~ s/^.*([0-9]{4})\..*$/$1/r;
 $resource->{cover} = { file => $file, hash => $key };
 $hash{$key} = 1;
 
-#
-# 写真情報を読み込む
-#
+# load photo's metadata
 my @photos_unsorted = ();
-foreach my $file (glob "$material_img/$cid/????[0-9][0-9][0-9][0-9].*") {
+foreach my $file (glob "$material_img/$cid/*[0-9][0-9][0-9][0-9].*") {
   my $base = basename($file);
   my $info = ImageInfo($file);
   my $t = Time::Piece->strptime($info->{DateTimeOriginal}, '%Y:%m:%d %T');
@@ -258,6 +212,7 @@ foreach my $file (glob "$material_img/$cid/????[0-9][0-9][0-9][0-9].*") {
     height => $info->{ImageHeight},
     caption => decode_utf8($info->{Title}) || $base
   };
+  # shorten photo filename to 4-digit number
   my $key = $base =~ s/^....(....)\..*$/$1/r;
   if (exists $hash{$key} && $key ne $resource->{cover}->{hash}) {
     for (my $i = 0; $i <= 26; $i++) {
@@ -275,9 +230,7 @@ foreach my $file (glob "$material_img/$cid/????[0-9][0-9][0-9][0-9].*") {
 }
 my @photos = sort { $a->{time} cmp $b->{time} } @photos_unsorted;
 
-#
-# 写真をセクションに振り分け
-#
+# assign photo to section by timestamp
 my $n = $#{$resource->{section}};
 foreach my $photo (@photos) {
   my $t = $photo->{time};
@@ -297,11 +250,9 @@ foreach my $photo (@photos) {
   }
 }
 
-#
-# JSON出力
-#
+# output resource JSON
 my $json = "$content/$cid.json";
-open(my $out, '>', $json) or die "Can't open '$json' $!";
+open(my $out, '>', $json) or die "Can't open $json: $!";
 print $out to_json($resource, { pretty => 1 }), "\n";
 close($out);
 __END__
